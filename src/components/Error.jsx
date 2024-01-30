a582a1c7c2a1c7e36b57caec69d58f2fe9de5ad4
jgezziel <gzl1910@gmail.com>
2024-01-30 14:35:04 -0600

    Add component Error

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = ({children}) => {
+  return (
+    <div className=" bg-red-50 border-l-4 border-red-500 p-3 text-red-500 font-semibold rounded-e">
+        {children}
+    </div>
+  )
+}
+
+export default Error
