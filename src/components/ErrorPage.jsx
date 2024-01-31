a69b0f30f9b943de1a4693dd3846082d126cb5d2
jgezziel <gzl1910@gmail.com>
2024-01-31 11:59:25 -0600

    Update component errorPage

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
     <div className="text-center">
         <h2 className="text-4xl font-extrabold text-purple-800 mb-4">CRM - Clients</h2>
         <p className="text-xl font-semibold my-4">Ha ocurrido un error</p>
-        <p className="bg-red-50 mb-4 border border-red-100 rounded text-red-500 text-pretty py-2">{error.message}</p>
+        <p className="bg-red-50 mb-4 border border-red-100 rounded text-red-500 text-pretty py-2">{error.statusText || error.message}</p>
     </div>
   )
 }
