f4d26c9d3484325f12150779cbccb62eac8b89be
jgezziel <gzl1910@gmail.com>
2024-01-31 12:06:34 -0600

    Update button deleteClient

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,5 +1,16 @@
+import {useNavigate, Form, redirect} from 'react-router-dom'
+import {deleteClient} from '../data/clients'
+
+export const action = async ({params}) => {
+  await deleteClient(params.id)
+  return redirect('/')
+}
+
 const Client = ({ client }) => {
+  const navigate = useNavigate();
+
   const { id, name, email, phone, company } = client;
+
   return (
     <tr className="border-b">
       <td className="p-4">
@@ -11,12 +22,14 @@ const Client = ({ client }) => {
         <p className="text-purple-400"><span className="text-gray-600 font-semibold">Phone: </span>{phone}</p>
       </td>
       <td className="p-4 flex gap-4">
-        <button className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded">
+        <button onClick={()=>navigate(`/clients/${id}/edit`)} className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded">
           Edit
         </button>
-        <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
-          Delete
-        </button>
+        <Form method="POST" action={`/clients/${id}/delete`} onSubmit={(e)=>{!confirm('Are you sure you want to eliminate the client?') && e.preventDefault()}}>
+          <button type="submit" className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+            Delete
+          </button>
+        </Form>
       </td>
     </tr>
   );
